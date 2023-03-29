67168881f94e129ee2c9febb6001007206759979
“Jelly <jelly.lumactud@kodego.ph>
2023-03-29 16:30:54 +0800

    complete s02 session

--- vanillaJS/s02/script.js
+++ vanillaJS/s02/script.js
@@ -98,3 +98,150 @@ console.log( i *= 5); //40
 //Division-Assignment Operator (/=)
 console.log(i /= 2); //20
 
+//Modulus-Assignment Operator (%=)
+console.log (i %= 8); //4
+
+//Comparison Operators
+//compares two operands and returns a logical value (true or false)
+//relational: < - less than and > - greater than
+
+//Equality Operator or a.k.a "loose equality" (==)
+//COERCION IS ALLOWED
+//compares the same value REGARDLESS of data type because of coercion
+
+console.log("Jan" == "January"); //false - different strings of data
+console.log(true == true); //true
+console.log (false == true); //false
+console.log (true == false); //false
+console.log(false == false); //true - same value of boolean
+console.log (null == undefined); //true - both empty
+
+console.log ("20" == 20); //true - type coercion
+console.log (true == 1); //true - binary code
+console.log (true == 2); //false
+console.log (false == 0); //true - binary code
+
+//Type Coercion - automatic/implicit conversion of values from one data type to another.
+//parseInt() - for whole numbers
+//parseFloat() - for decimal numbers
+
+let ex = 75.68;
+console.log(parseInt("75.68")); //75 rounded as a whole number
+console.log(parseFloat("75.68")); //75.68 - it retains their decimal places
+console.log(parseInt("75.68").toFixed(2)); //it displays the number of decimals in the number that was declared
+console.log(parseFloat("75.68").toFixed(5)); //
+
+//Strict Equality Operator (===)
+//compares "sameness of value" and data type
+//NO COERCION ALLOWED
+
+console.log (1 === 1); //true
+console.log (5 === "5"); //false
+console.log (true === 1); //false
+console.log (null === undefined); //false
+console.log ("James" === "Daniel"); //false - different value
+console.log (true === true); //true
+console.log (false === true); //false - different value
+console.log (false === false); //true - same value
+console.log (true === false); //false
+
+//Inequality Operator (!=) a.k.a "Loose Inequality Operator"
+//it compares "difference of values"
+//coercion will take place
+
+console.log (false != true); //true
+console.log (3 != 3.00); //false
+console.log (null != undefined); //false
+console.log (true != 1); //false - they are equal and coercion takes place
+
+//Greater Than (>)
+console.log (100 > 75); //true
+console.log (95.75 > 95); //true
+
+//Less Than (<)
+console.log (45 < 50); //true
+console.log (67 < 45); //false
+
+//Greater Than or Equals To (>=)
+console.log (45 >= 45); //true
+console.log (44 >= 45); //false
+
+//Less Than or Equal To (<=)
+console.log (100 <= 100); //true
+console.log (1 <= 0.5); //false
+
+//Logical Operators 
+
+//AND (&&) Operator
+console.log (true && true); //true
+console.log (true && false); //false
+console.log (false && true); //false
+console.log (false && false); //false
+
+console.log (("Max" == "Max") && ( undefined == null)); //true
+            //(T && T )
+            //T
+
+let kei = true;
+let el = false;
+
+console.log (kei > 2 && kei < 2); //false
+            // F && T
+            //F
+
+console.log (kei == el); //false
+
+let isOnline = true;
+let isBoss = false;
+let chatSiBoss = isOnline && isBoss;
+console.log (chatSiBoss); //false
+
+//OR Operator (||)
+//it evaluates to true of either of the operation is true
+//if both operands are true result is true (vice versa: if both operands are false then the result is false)
+
+console.log (true || true); //true
+console.log (true || false); //true
+console.log (false || true); //true
+console.log (false || false); //false
+
+//NOT OPERATOR (!)
+console.log (!true); //false
+console.log (!false); //true
+
+let isAdmin = true;
+let isInstructor = "1";
+
+let isAnswer = !isAdmin === isInstructor;
+            //false === "1"
+            //F - no coercion
+console.log (isAnswer); //false
+
+let faveNum = 333;
+let chooseNum = 245;
+
+let isHigher = !(chooseNum > faveNum || faveNum >= chooseNum)
+             //= !(F || T)
+             // = !T
+             //= False
+console.log (isHigher); //false
+
+/*
+
+Exercise:
+
+!
+(!isFalse != !isFalse && isFalse !== isTrue || 
+T != T && F !== T
+F && F
+= F
+    
+(!isTrue != !isFalse && isFalse != !isTrue));
+F != T && F != F
+T && F
+F
+T || F
+!F
+= True
+
+*/
