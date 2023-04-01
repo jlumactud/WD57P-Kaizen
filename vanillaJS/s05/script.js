88f84759aa2b44c4b0c82acf390b853ee044ce43
“Jelly <jelly.lumactud@kodego.ph>
2023-04-01 11:47:27 +0800

    Nested Loops

--- vanillaJS/s05/script.js
+++ vanillaJS/s05/script.js
@@ -67,23 +67,37 @@ console.log("connected");
 //     }
 // }
 
-function multiplyAll (arr)
+// function multiplyAll (arr)
+// {
+//     let product = 1;
+
+//     for (let i=0; i <arr.length; i++)
+//     {
+//         for (let j=0; j < arr[i].length; j++)
+//         {
+//             product *= arr[i][j];
+//         }
+//     }
+
+//     console.log(product);
+//     return product
+// }
+
+// //invoke the function
+// multiplyAll([[2,5],[9,10],[11,17,20]]);
+
+//SOLUTION
+const name = ["Karen", "Marites", "Marisol"];
+
+//outer loop starts here
+for (let i = 0; i <name.length; i++)
 {
-    let product = 1;
-
-    for (let i=0; i <arr.length; i++)
+    console.log(`My name is ${name[i]}`);
+    //inner loop
+    for (let j = 0; j < name[i].length; j++)
     {
-        for (let j=0; j < arr[i].length; j++)
-        {
-            product *= arr[i][j];
-        }
+        console.log(`spells ${name[i][j]}`)
     }
-
-    console.log(product);
-    return product
 }
-
-//invoke the function
-multiplyAll([[2,5],[9,10],[11,17,20]]);
 
 
